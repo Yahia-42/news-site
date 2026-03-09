097d69d31c5f5472549d5d8e10e3942577cb36d4
Yahia-42 <ya7ia99@gmail.com>
2026-03-09 11:20:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cron = require('node-cron');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
